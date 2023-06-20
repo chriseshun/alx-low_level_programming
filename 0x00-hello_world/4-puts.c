d6fe0a093b7450c3f964c47d7ecc8a19ef23dfd3
chriseshun <eshunchris12@gmail.com>
2023-06-20 08:21:35 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,6 @@
  * description: printing using the 'put' function
  * Return: Always end with 0
  */
-
 int main(void)
 { puts("\"Programming is like building a multilingual puzzle");
     return (0);
