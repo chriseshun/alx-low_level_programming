86ca24437599f76164088fb86446039555cfb316
chriseshun <eshunchris12@gmail.com>
2023-06-20 08:05:04 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  * description: printing using the 'put' function
  * Return
+ */
+
 int main(void)
 {puts("\"Programming is like building a multilingual puzzle");
     return 0; 
