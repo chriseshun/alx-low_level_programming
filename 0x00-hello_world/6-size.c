39d03b33edf0787aa6a38ad7f1ab87787bc10389
chriseshun <eshunchris12@gmail.com>
2023-06-20 16:29:19 -0700

    task6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - the starting point
+ * Description: prints the size of various types
+ * Return: Always end with 0
+ */
+Int main(void)
+{ printf("size of a char: %c bytes\n", size of (char));
+  ptintf("size of an int: %d bytes\n", size of (int));
+  printf("size of a long: %ld bytes\n", size of (long));
+  printf("size of a long long int: %lld bytes\n", size of (long long int));
+  printf("size of a float: %f bytes\n", size of (float));
+  
+  Return (0);
+}
