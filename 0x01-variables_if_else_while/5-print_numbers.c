98d855bbc41509e5154cececbd72bc47a5e5a502
chriseshun <eshunchris12@gmail.com>
2023-06-21 14:24:45 -0700

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: prints all single digit numbers
+ * Return: Always end with 0
+ *
+ */
+int main(void)
+{
+int number;
+for (number = 0; number < 10; number++)
+{ putchar(number + '0'}; }
+putchar('\n');
+return (0);
+}
