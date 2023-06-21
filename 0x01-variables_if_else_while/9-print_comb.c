277bba0fcab9689d7aecffb926925615886fed75
chriseshun <eshunchris12@gmail.com>
2023-06-21 15:32:43 -0700

    task9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,12 +9,11 @@
 int main(void)
 {
 int i;
-for (i = 0; i < 10; i++) {
+for (i = 0; i < 9; i++) {
 putchar(i + '0');
-if (i != 9) {
 putchar(',');
 putchar(' ');
 }
-}
+putchar('9');
 return (0);
 }
