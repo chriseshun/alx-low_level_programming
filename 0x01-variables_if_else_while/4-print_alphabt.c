2dc4fd93a99e4748e48fbc4590a59361c04718b2
chriseshun <eshunchris12@gmail.com>
2023-06-21 13:04:28 -0700

    task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: printing lowercase using putchar
+ * Return: always end with 0
+ *
+ */
+int main(void)
+{
+char letter
+for (letter = 'a'; letter <= 'z'; letter++)
+{ if (letter != 'q' && letter != 'e')
+{ putchar(letter); }
+}
+putchar('\n');
+return (0);
+}
