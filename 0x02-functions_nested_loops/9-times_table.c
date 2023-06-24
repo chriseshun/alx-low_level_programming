af8b1d86342e99424747febfecf3d7e63df2eaac
chriseshun <eshunchris12@gmail.com>
2023-06-24 09:46:04 -0700

    task9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ */
+void times_table(void)
+{
+int row, column, product;
+for (row = 0; row < 10; row++) {
+for (column = 0; column < 10; column++) {
+product = column * row;
+if (column == 0) {
+_putchar(product + '0');
+}
+if (product < 10 && column != 0) {
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(product + '0');
+}
+else if (product >= 10) {
+_putchar(',');
+_putchar(' ');
+_putchar((product / 10) + '0');
+_putchar((product % 10) + '0');
+}
+}
+_putchar('\n');
+}
+}
