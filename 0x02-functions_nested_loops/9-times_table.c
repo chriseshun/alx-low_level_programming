5d63bcc91909f8f3335e488c50b54ddbf37c3ed6
chriseshun <eshunchris12@gmail.com>
2023-06-24 09:48:28 -0700

    task9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,13 +12,15 @@ product = column * row;
 if (column == 0) {
 _putchar(product + '0');
 }
-if (product < 10 && column != 0) {
+if (product < 10 && column != 0) 
+{
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar(product + '0');
 }
-else if (product >= 10) {
+else if (product >= 10)
+{
 _putchar(',');
 _putchar(' ');
 _putchar((product / 10) + '0');
