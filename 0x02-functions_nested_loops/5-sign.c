02badd319ef269d8415f6e5704b692bd5fa1a9f0
chriseshun <eshunchris12@gmail.com>
2023-06-24 09:02:58 -0700

    task

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,13 +9,16 @@
  */
 int print_sign(int num)
 {
-if (num > 0){
+if (num > 0)
+{
 _putchar('+');
-return (1);}
-else if (num == 0){
+return (1); }
+else if (num == 0)
+{
 _putchar('0');
-return (0);}
-else {
+return (0); }
+else
+{
 _putchar('-');
-return (-1);}
+return (-1); }
 }
