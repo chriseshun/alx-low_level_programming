718c4af5d22159dcfcc553814e34dead95fdb0e1
chriseshun <eshunchris12@gmail.com>
2023-06-24 09:28:00 -0700

    task7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -18,7 +18,7 @@ last_digit = num % 10;
 if (last_digit < 0)
 last_digit = -last_digit;
 
-_putchar(last_digit + '0') ;
+_putchar(last_digit + '0');
 
-return (last_digit) ;
+return (last_digit);
 }
