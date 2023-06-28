2795a49efc336e1bba6017ecdd31430aaf652edd
chriseshun <eshunchris12@gmail.com>
2023-06-28 11:49:31 -0700

    task1

---
+++ 0x04-more_functions_nested_loops/1-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main()
+{
+char ch = '5';
+if (_isdigit(ch))
+{
+printf("The character is a digit.\n");
+}
+else
+{
+printf("The character is not a digit.\n");
+}
+return (0);
+}
