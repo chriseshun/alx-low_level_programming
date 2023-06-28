2d6ac69bac4438613960dc8414a2a5de3f9f35fa
chriseshun <eshunchris12@gmail.com>
2023-06-28 13:56:05 -0700

    task

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,17 +6,15 @@
  */
 void print_line(int n)
 {
-    if (n <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
-
-    while (n > 0)
-    {
-        _putchar('_');
-        n--;
-    }
-
-    _putchar('\n');
+if (n <= 0)
+{
+_putchar('\n');
+return ;
+}
+while (n > 0)
+{
+_putchar('_');
+n--;
+}
+_putchar('\n');
 }
