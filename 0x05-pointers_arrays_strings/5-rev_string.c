9e6525e4174b35610e31ada805f4d321b99f8fe7
chriseshun <eshunchris12@gmail.com>
2023-06-29 07:07:40 -0700

    task

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,19 +6,17 @@
  */
 void rev_string(char *str)
 {
-    char temp = str[0];
-    int length = 0;
-    int counter = 0;
-
-    while (str[length] != '\0')
-        length++;
-
-    for (counter = 0; counter < length; counter++)
-    {
-        length--;
-        temp = str[counter];
-        str[counter] = str[length];
-        str[length] = temp;
-    }
+char temp = str[0];
+int length = 0;
+int counter = 0;
+while (str[length] != '\0')
+length++;
+for (counter = 0; counter < length; counter++)
+{
+length--;
+temp = str[counter];
+str[counter] = str[length];
+str[length] = temp;
+}
 }
 
