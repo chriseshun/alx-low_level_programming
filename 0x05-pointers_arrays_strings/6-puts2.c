4224153d09442ddc17e75559bb289ae2db2da807
chriseshun <eshunchris12@gmail.com>
2023-06-29 07:14:41 -0700

    task

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,12 +8,15 @@
  */
 void puts2(char *str)
 {
-    int i = 0;
-
-    while (str[i] != '\0')
+    int index = 0;
+    
+    while (str[index] != '\0')
     {
-        _putchar(str[i]);
-        i += 2;
+        if (index % 2 == 0)
+        {
+            _putchar(str[index]);
+        }
+        index++;
     }
     
     _putchar('\n');
