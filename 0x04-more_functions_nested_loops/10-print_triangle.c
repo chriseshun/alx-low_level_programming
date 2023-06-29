29c7e0b0f3cc1053ffddcb4be676f6913fe8b327
chriseshun <eshunchris12@gmail.com>
2023-06-29 01:27:02 -0700

    task

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -3,30 +3,21 @@
  * print_number - prints a triangle
  * @size: size to be printed
  */
-void print_triangle(int size)
-
+void print_triangle(int size) 
 {
-	if (size <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		int n, m;
-
-		for (n = 1; n <= size; n++)
-		{
-			for (m = n; m < size; m++)
-			{
-				_putchar(' ');
-			}
-
-			for (m = 1; m <= n; m++)
-			{
-				_putchar('#');
-			}
-
-			_putchar('\n');
-		}
-	}
+if (size <= 0) {
+_putchar('\n');
+}
+else {
+int n, m;
+for (n = 1; n <= size; n++) {
+for (m = n; m < size; m++) {
+_putchar(' ');
+}
+for (m = 1; m <= n; m++) {
+_putchar('#');
+}
+_putchar('\n');
+}
+}
 }
