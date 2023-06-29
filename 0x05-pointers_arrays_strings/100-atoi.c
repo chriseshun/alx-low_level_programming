2d365e674e3e2211f3468d85c87c6595e71dd2bb
chriseshun <eshunchris12@gmail.com>
2023-06-29 08:09:54 -0700

    task

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * _atoi - convert a string to integer
+ * @str: Pointer to convert
+ * Return : integer
+ */
+int _atoi(char *str)
+{
+int index = 0;
+unsigned int number = 0;
+int sign = 1;
+int isNumber = 0;
+
+while (str[index])
+{
+if (str[index] == '-')
+{
+sign *= -1;
+}
+
+while (str[index] >= '0' && str[index] <= '9')
+{
+isNumber = 1;
+number = (number * 10) + (str[index] - '0');
+index++;
+}
+
+if (isNumber == 1)
+{
+break;
+}
+
+index++;
+}
+
+number *= sign;
+return (number);
+}
