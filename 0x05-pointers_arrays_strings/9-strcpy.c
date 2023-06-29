30deab187972ae633746a059f396f3edf48f91de
chriseshun <eshunchris12@gmail.com>
2023-06-29 07:43:21 -0700

    task

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strcpy- copy the string pointed 
+ * _strcpy- copy the string pointed
  * @destination: char to check
  * @source: char to check
  * Return: Always end with 0
