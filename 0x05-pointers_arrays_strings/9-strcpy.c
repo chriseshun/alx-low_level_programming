32768f15abf31e1d906a42fa47b0c17d0954d6f5
chriseshun <eshunchris12@gmail.com>
2023-06-29 07:34:08 -0700

    task

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strcpy- copy the string pointed 
+ * @destination: char to check
+ * @source: char to check
+ * Return: Always end with 0
+ */
+char *_strcpy(char *destination, char *source)
+{
+    int index;
+
+    for (index = 0; source[index] != '\0'; index++)
+        destination[index] = source[index];
+
+    destination[index] = '\0';
+    return (destination);
+}
