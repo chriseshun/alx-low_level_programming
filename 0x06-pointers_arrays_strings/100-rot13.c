0a5d167c7cbfbe3c3f7d42d6fbf7cfe473802b21
chriseshun <eshunchris12@gmail.com>
2023-06-30 03:23:07 -0700

    task

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * rot13 - Encodes a string using ROT13
+ * @str: Pointer to the string to be encoded
+ *
+ * Return: Pointer to the encoded string
+ */
+char *rot13(char *str)
+{
+char *ptr = str;
+char *uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char *lowercase = "abcdefghijklmnopqrstuvwxyz";
+char *rot13_uppercase = "NOPQRSTUVWXYZABCDEFGHIJKLM";
+char *rot13_lowercase = "nopqrstuvwxyzabcdefghijklm";
+int i;
+
+while (*ptr != '\0')
+{
+for (i = 0; i < 26; i++)
+{
+if (*ptr == uppercase[i])
+{
+*ptr = rot13_uppercase[i];
+break;
+} else if (*ptr == lowercase[i])
+{
+*ptr = rot13_lowercase[i];
+break;
+}
+}
+ptr++;
+}
+
+return (str);
+}
