229c214d91a41cfb7501e35c9ecb01e1a152673f
chriseshun <eshunchris12@gmail.com>
2023-07-02 09:45:45 -0700

    task

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -7,65 +7,49 @@
  * @r: Buffer to store the result
  * @size_r: Size of the buffer
  *
- * Return: Pointer to the result, or 0 if the result cannot be stored in r
+ * Return: Pointer to the result
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int carry = 0;
-int i = 0;
-int sum = 0;
+int overflow = 0, i = 0, j = 0, digits = 0;
+int val1 = 0, val2 = 0, temp_tot = 0;
 
-int len1 = 0, len2 = 0;
-while (n1[len1] != '\0')
-len1++;
-while (n2[len2] != '\0')
-len2++;
-
-if (len1 >= size_r || len2 >= size_r)
-return (0);
-
-while (len1 > 0 || len2 > 0)
-{
-sum = carry;
-if (len1 > 0)
-sum += (n1[len1 - 1] - '0');
-if (len2 > 0)
-sum += (n2[len2 - 1] - '0');
-
-carry = sum / 10;
-
-if (i >= size_r - 1)
-return (0);
-
-r[i] = (sum % 10) + '0';
-
+while (*(n1 + i) != '\0')
 i++;
-len1--;
-len2--;
-}
-
-if (carry > 0)
-{
-if (i >= size_r - 1)
-return (0);
-
-r[i] = carry + '0';
-i++;
-}
-
-r[i] = '\0';
-
-int j = 0;
-i--;
-while (j < i)
-{
-char temp = r[j];
-r[j] = r[i];
-r[i] = temp;
+while (*(n2 + j) != '\0')
 j++;
 i--;
+j--;
+if (j >= size_r || i >= size_r)
+return (0);
+while (j >= 0 || i >= 0 || overflow == 1)
+{
+if (i < 0)
+val1 = 0;
+else
+val1 = *(n1 + i)-'0';
+if (j < 0)
+val2 = 0;
+else
+val2 = *(n2 + j)-'0';
+temp_tot = val1 + val2 + overflow;
+if (temp_tot >= 10)
+overflow = 1;
+else
+overflow = 0;
+if (digits >= (size_r - 1))
+return (0);
+*(r + digits) = (temp_tot % 10) + '0';
+digits++;
+j--;
+i--;
 }
-
+if (digits == size_r)
+{
+return (0);
+}
+*(r + digits) = '\0';
+rev_string(r);
 return (r);
 }
