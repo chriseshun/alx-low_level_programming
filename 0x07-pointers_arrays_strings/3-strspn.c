34acedc2bf796bc278f1a4bf642c2c58fcaaa0ef
chriseshun <eshunchris12@gmail.com>
2023-07-04 11:25:00 -0700

    task

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 
 /**
  * _strspn - Gets the length of a prefix substring.
@@ -15,7 +15,8 @@ while (*s && is_accept)
 {
 char *a = accept;
 is_accept = 0;
-while (*a) {
+while (*a)
+{
 if (*s == *a)
 {
 is_accept = 1;
