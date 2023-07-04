85b07afbc1befb50a6fc715f97549ee439a47ec9
chriseshun <eshunchris12@gmail.com>
2023-07-04 12:32:36 -0700

    task

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -3,7 +3,7 @@
 
 /**
  * print_diagsums - Prints the sum of the two diagonals of a square matrix.
- *@a The square matrix represented as a 1D array
+ * @a: The square matrix represented as a 1D array
  * @size: The size of the square matrix.
  */
 void print_diagsums(int *a, int size)
