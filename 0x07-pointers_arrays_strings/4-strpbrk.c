c1878f231e6746d6a78b7e5a5724c008ff290c97
chriseshun <eshunchris12@gmail.com>
2023-07-04 11:58:58 -0700

    task

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,12 +10,16 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-unsigned int i, j;
-for (i = 0; s[i] != '\0'; i++)
+while (*s)
 {
-for (j = 0; accept[j] != '\0'; j++)
+char *a = accept;
+while (*a)
 {
-if (s[i] == accept[j])
-return (s + i);
+if (*s == *a)
+return (s);
+a++;
 }
+s++;
+}
+return (NULL);
 }
