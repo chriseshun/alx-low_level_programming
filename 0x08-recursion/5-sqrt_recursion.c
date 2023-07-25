38749fc14481c1dbc1a19c910da51b9514fab2a8
chriseshun <eshunchris12@gmail.com>
2023-07-25 16:05:55 -0700

    task

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,43 +2,31 @@
 #include <math.h>
 
 /**
- * _sqrt_recursion - Returns the natural square root of a number using recursion.
- * @n: The input number.
+ * square_root_check - Recursively checks for the square root of a number.
+ * @guess: The current guess for the square root.
+ * @number: The number to find the square root of.
  *
- * Return: The natural square root of n, or -1 if n does not have a natural square root.
+ * Return: -1 if there is no natural square root, otherwise return the square root.
+ */
+
+int square_root_check(int guess, int number)
+{
+if (guess * guess == number)
+return (guess);
+if (guess * guess > number)
+return (-1);
+return (square_root_check(guess + 1, number));
+}
+
+/**
+ * _sqrt_recursion - Returns the natural square root of a number using recursion.
+ * @n: The integer to find the square root of.
+ *
+ * Return: The natural square root of n, or -1
  */
 int _sqrt_recursion(int n)
 {
-if (n < 0)
-return (-1);
-
-if (n == 0 || n == 1)
-return (n);
-
-return (helper_sqrt(n, 1, n));
-}
-/**
- * helper_sqrt - Helper function to find the natural square root using binary search.
- * @n: The input number.
- * @start: The starting value for binary search.
- * @end: The ending value for binary search.
- *
- * Return: The natural square root of n, or -1 if n does not have a natural square root.
- */
-int helper_sqrt(int n, int start, int end)
-{
-if (start <= end)
-{
-int mid = (start + end) / 2;
-
-if (mid * mid == n)
-return (mid);
-
-if (mid * mid > n)
-return (helper_sqrt(n, start, mid - 1));
-
-return (helper_sqrt(n, mid + 1, end));
-}
-
-return (end);
+if (n == 0)
+return (0);
+return (square_root_check(1, n));
 }
