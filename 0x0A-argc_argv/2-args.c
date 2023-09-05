5bae9f417cf7963a8944606b3634879c9e5a9c10
chriseshun <eshunchris12@gmail.com>
2023-09-05 07:41:37 -0700

    task

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main- entry
+ * @argc: number of input
+ * @argv: array of string values
+ * 
+ * Return: 0 success
+ */
+
+int main(int argc, char *argv[])
+{
+int i;
+for (i = 0; i < argc; i++)
+printf("%s\n", argv[i]);
+return (0);
+}
