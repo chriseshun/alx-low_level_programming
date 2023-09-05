7f4858c62a91fca6a5f0d0fc4ca81fb10becd7ed
chriseshun <eshunchris12@gmail.com>
2023-09-04 17:43:04 -0700

    task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - entry point
+ * @argc: number of input
+ * @argv: An Array of string values
+ *
+ * Return: 0 sucess
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc - 1);
+return (0);
+}
