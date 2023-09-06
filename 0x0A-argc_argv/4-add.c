2b4941a13ba251e68499563439b346ff2839e64a
chriseshun <eshunchris12@gmail.com>
2023-09-06 16:59:25 -0700

    task

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,17 +12,32 @@
 
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
-	if (argc < 1) 
-	{
-		return (0);
-	}
-	if (argc > 1)
-	{
-		for (i = 1; i < argc; i++)
-		{
-			sum += atoi(argv[i]);
-		}
-	}
-	return (0);
+    if (argc == 1)
+    {
+        printf("0\n");
+        return (0);
+    }
+
+    int sum = 0;
+
+    for (int i = 1; i < argc; i++)
+    {
+        char *number_str = argv[i];
+
+        for (int j = 0; number_str[j] != '\0'; j++)
+        {
+            if (!isdigit(number_str[j]))
+            {
+                printf("Error\n");
+                return (1);
+            }
+        }
+
+        int number = atoi(number_str);
+        sum += number;
+    }
+
+    printf("%d\n", sum);
+
+    return (0);
 }
