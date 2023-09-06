11b5be5e67fdb811f496883c8f17530785f8d34d
chriseshun <eshunchris12@gmail.com>
2023-09-06 16:49:44 -0700

    task

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry
+ * @argc: number of entry
+ * @argv: array of string values
+ * Return: 0 success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+	if (argc < 1) 
+	{
+		return (0);
+	}
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			sum += atoi(argv[i]);
+		}
+	}
+	return (0);
+}
