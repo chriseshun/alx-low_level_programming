f163b481556f7b267789e06156b61f5772197e70
chriseshun <eshunchris12@gmail.com>
2023-09-06 18:11:43 -0700

    task

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,29 +9,23 @@
 
 char *_strdup(char *str)
 {
-    int a = 0;
-    int b = 1;
-    char *thread;
-
-    if (str == NULL)
-        return (NULL);
-
-    while (str[b])
-    {
-        b++;
-    }
-
-    thread = malloc((sizeof(char) * b) + 1);
-
-    if (thread == NULL)
-        return (NULL);
-
-    while (a < b)
-    {
-        *(thread + a) = *(str + a);
-        a++;
-    }
-
-    thread[a] = '\0';
-    return (thread);
+int a = 0;
+int b = 1;
+char *thread;
+if (str == NULL)
+return (NULL);
+while (str[b])
+{
+b++;
+}
+thread = malloc((sizeof(char) * b) + 1);
+if (thread == NULL)
+return (NULL);
+while (a < b)
+{
+*(thread + a) = *(str + a);
+a++;
+}
+thread[a] = '\0';
+return (thread);
 }
