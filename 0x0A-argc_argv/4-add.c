0b250a6b150dc5d9a51b2ab993905f5334d1ec0a
chriseshun <eshunchris12@gmail.com>
2023-09-06 17:20:55 -0700

    task

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,43 +1,50 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
- * main - entry
- * @argc: number of entry
- * @argv: array of string values
+ * main - Entry point
+ * @argc: Number of input arguments
+ * @argv: Array of string values
  *
- * Return: 0 success
+ * Return: 0 for success, 1 for error
  */
-
 int main(int argc, char *argv[])
 {
-    if (argc == 1)
-    {
-        printf("0\n");
-        return (0);
-    }
+	int i, sum = 0;
 
-    int sum = 0;
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
 
-    for (int i = 1; i < argc; i++)
-    {
-        char *number_str = argv[i];
+	for (i = 1; i < argc; i++)
+	{
+		char *arg = argv[i];
+		int j;
 
-        for (int j = 0; number_str[j] != '\0'; j++)
-        {
-            if (!isdigit(number_str[j]))
-            {
-                printf("Error\n");
-                return (1);
-            }
-        }
+		for (j = 0; arg[j] != '\0'; j++)
+		{
+			if (!isdigit(arg[j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
 
-        int number = atoi(number_str);
-        sum += number;
-    }
+		int num = atoi(arg);
 
-    printf("%d\n", sum);
+		if (num < 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
 
-    return (0);
+		sum += num;
+	}
+
+	printf("%d\n", sum);
+	return (0);
 }
+
