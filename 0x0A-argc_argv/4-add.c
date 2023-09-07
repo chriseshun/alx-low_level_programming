dd5389c57d1fc857ea79c76cfcfdc786281785f1
chriseshun <eshunchris12@gmail.com>
2023-09-06 17:00:57 -0700

    task

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,12 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 
 /**
  * main - entry
  * @argc: number of entry
  * @argv: array of string values
+ *
  * Return: 0 success
  */
 
