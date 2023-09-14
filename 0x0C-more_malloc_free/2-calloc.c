4f5f67872f20ec88687f618c8942aa583e540871
chriseshun <eshunchris12@gmail.com>
2023-09-13 19:11:48 -0700

    corrections on betty style on codes

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -5,27 +5,21 @@
  * _calloc - Allocates memory for an array
  * @nmemb: Number of elements
  * @size: Size of each element
- *
  * Return: A pointer to the allocated memory, or NULL if it fails
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    unsigned int x, y;
-    char *q;
-
-    if (nmemb == 0 || size == 0)
-        return (NULL);
-
-    y = nmemb * size;
-    q = malloc(y);
-
-    if (q == NULL)
-        return (NULL);
-    
-    for (x = 0; x < y; x++)
-    {
-        *(q + x) = 0;
-    }
-    
-    return (q);
+unsigned int x, y;
+char *q;
+if (nmemb == 0 || size == 0)
+return (NULL);
+y = nmemb * size;
+q = malloc(y);
+if (q == NULL)
+return (NULL);
+for (x = 0; x < y; x++)
+{
+*(q + x) = 0;
+}
+return (q);
 }
--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -6,32 +6,26 @@
  * array_range - Creates an array of integers.
  * @min: Minimum input
  * @max: Maximum input
- *
  * Return: A pointer to the allocated array, or NULL if it fails
  */
 int *array_range(int min, int max)
 {
-    int *T, N = 0; // Changed a to T and m to N
-    int sum;
-
-    if (min > max)
-    {
-        return (NULL);
-    }
-
-    sum = (max - min) + 1;
-    T = malloc(sizeof(int) * sum);
-
-    if (T == NULL)
-    {
-        return (NULL);
-    }
-    
-    while (min <= max)
-    {
-        *(T + N) = min++;
-        N++;
-    }
-    
-    return (T);
+int *T, N = 0;
+int sum;
+if (min > max)
+{
+return (NULL);
+}
+sum = (max - min) + 1;
+T = malloc(sizeof(int) * sum);
+if (T == NULL)
+{
+return (NULL);
+}
+while (min <= max)
+{
+*(T + N) = min++;
+N++;
+}
+return (T);
 }
