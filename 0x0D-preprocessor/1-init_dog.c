79a52cebb44fb5ccd6d7047f8d011475191e6d93
chriseshun <eshunchris12@gmail.com>
2023-09-25 06:48:04 -0700

    task

---
+++ 0x0D-preprocessor/1-init_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdlib.h>
+
+
+/**
+*init_dog - a function that initializes a variable of type struct dog
+*@d: is a pointer
+*@name: input value
+*@age: input
+*@owner: input
+*
+* Return: 0 (success)
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	}
+
+}
