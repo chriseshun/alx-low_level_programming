e60850947f0698c34bd61f583a505e1dea89c37a
chriseshun <eshunchris12@gmail.com>
2023-09-25 05:41:08 -0700

    task

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - Entry point
+ * @h: Pointer
+ *
+ * Return : Output
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t G = 0;
+
+		while (h != NULL)
+	{
+		h = h->next;
+
+		G++;
+	}
+	
+	return (G);
+}
