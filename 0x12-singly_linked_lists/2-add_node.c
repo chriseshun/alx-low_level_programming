e6fba8627237e6409ad7965f91e76d54dae64a1b
chriseshun <eshunchris12@gmail.com>
2023-09-25 05:52:34 -0700

    task

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,54 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * _strlen - Computes the length of a string.
+ * @s: The string to calculate the length of.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(const char *s)
+{
+int length = 0;
+
+while (*s)
+{
+s++;
+length++;
+}
+    
+return (length);
+}
+
+/**
+ * add_node - Adds a new node at the beginning of a linked list.
+ * @head: A pointer to a pointer to the head of the list.
+ * @str: The string to be duplicated and stored in the new node.
+ *
+ * Return: The address of the new element, or NULL if it fails.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+list_t *new_node;
+
+if (head != NULL && str != NULL)
+{
+new_node = malloc(sizeof(list_t));
+if (new_node == NULL)
+return (NULL);
+
+new_node->len = _strlen(str);
+new_node->str = strdup(str);
+if (new_node->str == NULL)
+{
+free(new_node);
+return (NULL);
+}
+
+new_node->next = *head;
+*head = new_node;
+
+return (new_node);
+}
+return (NULL);
+}
