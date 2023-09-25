b7f6ded6194ef01bf9b7ed13cc214fa380093f4e
chriseshun <eshunchris12@gmail.com>
2023-09-25 07:17:34 -0700

    task

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,44 +1,40 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * main - Calculate minimum number of coins for change
- * @argc: Number of command-line arguments
- * @argv: Array of command-line argument strings
+ * main - prints minimum number of coins to make change for an amount of money.
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
  *
- * Return: 0 for success, 1 for error
+ * Return: 0 if no errors, else 1
  */
 int main(int argc, char *argv[])
 {
-    if (argc != 2)
-    {
-        printf("Error\n");
-        return (1);
-    }
+	int a, n = 0, i, t;
+	int c[5] = {25, 10, 5, 2, 1};
 
-    int cents = atoi(argv[1]);
-
-    if (cents < 0)
-    {
-        printf("0\n");
-        return (0);
-    }
-
-    int coins[] = {25, 10, 5, 2, 1};
-    int num_coins = sizeof(coins) / sizeof(coins[0]);
-    int count = 0;
-
-    for (int i = 0; i < num_coins; i++)
-    {
-        while (cents >= coins[i])
-        {
-            cents -= coins[i];
-            count++;
-        }
-    }
-
-    printf("%d\n", count);
-
-    return (0);
+	if (argc != 2)
+	{
+		puts("Error");
+		return (1);
+	}
+	a = atoi(argv[1]);
+	if (a <= 0)
+	{
+		puts("0");
+		return (1);
+	}
+	else
+	{
+		for (i = 0; i < 5; i++)
+		{
+			t = a / c[i];
+			a -= t * c[i];
+			n += t;
+			if (a == 0)
+				break;
+		}
+	}
+	printf("%d\n", n);
+	return (0);
 }
