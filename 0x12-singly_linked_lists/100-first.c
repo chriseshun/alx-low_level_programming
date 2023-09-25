bd9d364dafb9d5b242da803e8e8e1fd789cbb5b2
chriseshun <eshunchris12@gmail.com>
2023-09-25 06:24:12 -0700

    task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * starter - Entry point of the program.
+ *
+ */
+void starter(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
