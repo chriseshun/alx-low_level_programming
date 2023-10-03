522ab257189ae0d2668f73119702e34b375b075a
chriseshun <eshunchris12@gmail.com>
2023-10-02 18:34:10 -0700

    task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,21 +9,21 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-int file_descriptor = 0;
+int file_descrip = 0;
 int text_length = 0;
 
 if (filename == NULL)
 return (-1);
-file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-if (file_descriptor == -1)
+file_descrip = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+if (file_descrip == -1)
 return (-1);
 if (text_content)
 {
 while (text_content[text_length] != '\0')
 ++text_length;
-if (write(file_descriptor, text_content, text_length) == -1)
+if (write(file_descrip, text_content, text_length) == -1)
 return (-1);
 }
-close(file_descriptor);
+close(file_descrip);
 return (1);
 }
