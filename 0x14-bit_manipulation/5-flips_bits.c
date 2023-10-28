f190f3f626954575e490fd682e2a4e1b257f4ec5
chriseshun <eshunchris@gmail.com>
2023-10-27 19:14:07 -0700

    working on task

---
+++ 0x14-bit_manipulation/5-flips_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+/**
+ * flip_bits - returns bit value required to flip numbers
+ * @n: first num
+ * @m: 2nd num
+ * Return: total num of bits
+ */
+
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int newvalue = 0;
+
+	for (; n || m; n >>= 1, m >>= 1)
+	{
+		if ((n & 1) != (m & 1))
+			newvalue++;
+	}
+	return (newvalue);
+}
